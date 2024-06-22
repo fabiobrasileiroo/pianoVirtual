4b04af3f581674c3816e56d6df8560aa79a10bfc
fabiobrasileiroo <fabio.h591@gmail.com>
2024-06-22 18:44:48 -0400

    server.js from index.html

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 80;
+
+// Configura o diretório estático para servir arquivos
+app.use(express.static(path.join(__dirname, '/')));
+
+// Rota para servir o arquivo index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/', 'index.html'));
+});
+
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
